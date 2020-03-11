b878c053ea4e8a55d775d8f0af583fee7635da89
Blair <blairacarroll@gmail.com>
2020-03-11 19:44:38 -0400

    react router setup and style added

--- pilltracker/src/App.js
+++ pilltracker/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 // import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react';
 import MedicationIndex from './components/MedicationIndex';
 
 const App = () => (
   <div className="App">
-    <MedicationIndex />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={MedicationIndex} />
+      </Switch>
+    </BrowserRouter>
   </div>
 )
 
